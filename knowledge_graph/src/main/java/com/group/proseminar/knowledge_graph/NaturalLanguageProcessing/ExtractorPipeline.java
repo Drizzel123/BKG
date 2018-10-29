2af2ba9cf2120cbe827d3acb06c19eb9fe94c52d
Stefan Werner <36734964+StefanWerner97@users.noreply.github.com>
2018-10-29 16:31:44 +0100

    Refined object-post-processing

--- knowledge_graph/src/main/java/com/group/proseminar/knowledge_graph/NaturalLanguageProcessing/ExtractorPipeline.java
+++ knowledge_graph/src/main/java/com/group/proseminar/knowledge_graph/NaturalLanguageProcessing/ExtractorPipeline.java
@@ -1,9 +1,5 @@
 package com.group.proseminar.knowledge_graph.NaturalLanguageProcessing;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Properties;
 import java.util.Set;
 
@@ -15,8 +11,8 @@ public class ExtractorPipeline {
 
 	private StanfordCoreNLP corefPipeline;
 	private StanfordCoreNLP extrPipeline;
-	private final String CPROPERTIES = "tokenize,ssplit,pos,lemma,ner,parse";
-	private final String EPROPERTIES = "tokenize,ssplit,pos,lemma,ner,parse,dcoref";
+	private final String CPROPERTIES = "tokenize,ssplit,pos,lemma,ner,parse,dcoref";
+	private final String EPROPERTIES = "tokenize,ssplit,pos,lemma,ner,parse";
 	CoreferenceResoluter resoluter;
 	TripletExtractor extractor;
 
@@ -31,26 +27,20 @@ public class ExtractorPipeline {
 		// Initialize extrPipeline
 		Properties extrProps = new Properties();
 		extrProps.put("annotators", EPROPERTIES);
-		extrProps.put("dcoref.score", true);
-		this.extrPipeline = new StanfordCoreNLP(corefProps);
+		this.extrPipeline = new StanfordCoreNLP(extrProps);
 	}
-
-	public void startPipeline(String path) throws FileNotFoundException, IOException {
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			String line;
-			while ((line = br.readLine()) != null) {
-				Annotation doc = new Annotation(line);
-				corefPipeline.annotate(doc);
-				String resoluted = resoluter.coreferenceResolution(doc);
-				if (resoluted == null) {
-					resoluted = line;
-				}
-				Annotation res = new Annotation(resoluted);
-				extrPipeline.annotate(res);
-				Set<Triplet<Tree, Tree, Tree>> triplets = extractor.extractFromText(res);
-				// TODO: Write triplets to file
-				System.out.println("TRIPLETS: " + triplets);
-			}
+	
+	public void processArticle (String article) {
+		Annotation doc = new Annotation(article);
+		corefPipeline.annotate(doc);
+		String resoluted = resoluter.coreferenceResolution(doc);
+		if (resoluted == null) {
+			resoluted = article;
 		}
+		Annotation res = new Annotation(resoluted);
+		extrPipeline.annotate(res);
+		Set<Triplet<Tree, Tree, Tree>> triplets = extractor.extractFromText(res);
+		// TODO: Write triplets to file
+		System.out.println("TRIPLETS: " + triplets);
 	}
 }
--- knowledge_graph/src/main/java/com/group/proseminar/knowledge_graph/NaturalLanguageProcessing/TripletExtractor.java
+++ knowledge_graph/src/main/java/com/group/proseminar/knowledge_graph/NaturalLanguageProcessing/TripletExtractor.java
@@ -1,5 +1,7 @@
 package com.group.proseminar.knowledge_graph.NaturalLanguageProcessing;
 
+import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -7,31 +9,54 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.Stack;
 import java.util.stream.Collectors;
 
 import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.Label;
+import edu.stanford.nlp.ling.StringLabel;
 import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.trees.LabeledScoredTreeNode;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations;
+import edu.stanford.nlp.trees.TreeGraphNode;
+import edu.stanford.nlp.util.CoreMap;
 
 public class TripletExtractor {
 
-
+	/**
+	 * Extracts a set of triplets (subject, predicate, object) given an annotated
+	 * text.
+	 * 
+	 * @param annotation annotated text
+	 * @return set of triplets
+	 */
 	public Set<Triplet<Tree, Tree, Tree>> extractFromText(Annotation annotation) {
-		Tree parseTree = annotation.get(CoreAnnotations.SentencesAnnotation.class).get(0)
-				.get(TreeCoreAnnotations.TreeAnnotation.class);
 		Set<Triplet<Tree, Tree, Tree>> tripletSet = new HashSet<>();
-		// Start extraction on every "S" node
-		for (Tree node : parseTree) {
-			if (node.nodeString().split(" ")[0].equals("S")) {
-				tripletSet.addAll(extractTripletFromS(node));
+		List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+		for (CoreMap sentence : sentences) {
+			System.out.println("Sentence: " + sentence);
+
+			Tree parseTree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
+			// Start extraction on every "S" node
+			for (Tree node : parseTree) {
+				if (node.label().toString().equals("S")) {
+					tripletSet.addAll(extractTripletFromS(node));
+				}
 			}
 		}
 		return tripletSet;
 	}
 
+	/**
+	 * Extracts a set of triplets (subject, predicate, object) given the root of a
+	 * sentence.
+	 * 
+	 * @param sroot root of a sentence with label "S"
+	 * @return set of triplets derived from the dependency tree of the sentence
+	 */
 	public Set<Triplet<Tree, Tree, Tree>> extractTripletFromS(Tree sroot) {
 		Set<Triplet<Tree, Tree, Tree>> tripletSet = new HashSet<>();
 		Tree subj = extractSubjectFromS(sroot);
@@ -39,7 +64,7 @@ public class TripletExtractor {
 		if (pred == null || subj == null) {
 			return tripletSet;
 		}
-		Tree obj = extractObject(sroot, pred);
+		Tree obj = extractObjectOrigin(sroot, pred);
 		if (obj == null) {
 			return tripletSet;
 		}
@@ -49,10 +74,16 @@ public class TripletExtractor {
 		return tripletSet;
 	}
 
+	/**
+	 * Derives the subject of a sentence given the root.
+	 * 
+	 * @param sroot root of a sentence with label "S"
+	 * @return subject of the sentence
+	 */
 	public Tree extractSubjectFromS(Tree sroot) {
 		// Return null if S has no child with label NP
 		// Take subject from previous triplet???
-		if (!sroot.getChildrenAsList().stream().anyMatch(x -> x.nodeString().split(" ")[0].equals("NP"))) {
+		if (!sroot.getChildrenAsList().stream().anyMatch(x -> x.label().toString().equals("NP"))) {
 			return null;
 		}
 		Set<String> types = new HashSet<>();
@@ -78,7 +109,7 @@ public class TripletExtractor {
 		while (queue.size() != 0) {
 			node = queue.poll();
 			// Get type of node
-			String type = node.nodeString().split(" ")[0];
+			String type = node.label().toString();
 			if (types.contains(type)) {
 				return node;
 			}
@@ -102,7 +133,7 @@ public class TripletExtractor {
 	public Tree extractPredicateFromS(Tree sroot) {
 		// Find VP child from root S
 		Tree nodeVP = null;
-		List<Tree> listVP = sroot.getChildrenAsList().stream().filter(x -> x.nodeString().startsWith("VP"))
+		List<Tree> listVP = sroot.getChildrenAsList().stream().filter(x -> x.label().toString().equals("VP"))
 				.collect(Collectors.toList());
 		if (listVP.size() > 0) {
 			nodeVP = listVP.get(0);
@@ -133,18 +164,25 @@ public class TripletExtractor {
 		return deepestVT;
 	}
 
-	public Tree extractObject(Tree sroot, Tree vt) {
+	/**
+	 * Extracts the origin of the object with respect to the node of the predicate.
+	 * 
+	 * @param sroot root of the sentence with label "S"
+	 * @param vt    node of the predicate (verb type)
+	 * @return origin of the object
+	 */
+	public Tree extractObjectOrigin(Tree sroot, Tree vt) {
 		List<Tree> siblings = vt.siblings(sroot);
 		String types = "NP,PP,ADJP";
 		Set<String> typeSet = new HashSet<>(Arrays.asList(types.split(",")));
 		// Candidates are nodes of a type contained in typeSet
-		Set<Tree> candidates = siblings.stream().filter(tree -> typeSet.contains(tree.nodeString().split(" ")[0]))
+		Set<Tree> candidates = siblings.stream().filter(tree -> typeSet.contains(tree.label().toString()))
 				.collect(Collectors.toSet());
 
 		// Find object in candidates
 		Tree result = null;
 		for (Tree candidate : candidates) {
-			String type = candidate.nodeString().split(" ")[0];
+			String type = candidate.label().toString();
 			if (type.equals("NP") || type.equals("PP") || type.equals("S")) {
 				// Search for first noun
 				Set<String> nTypeSet = new HashSet<>();
@@ -163,30 +201,96 @@ public class TripletExtractor {
 		return result;
 	}
 
-	public Set<Tree> objectPostProcessing(Tree object) {
-		Set<Tree> candidates = null;
-		List<Tree> children = object.getChildrenAsList();
-		List<String> types = children.stream().map(x -> x.nodeString().split(" ")[0]).collect(Collectors.toList());
-		if (types.size() == 2) {
-			if (types.get(0).equals("NP")) {
-				if (types.get(1).equals("PP") || types.get(1).equals("SBAR")) {
-					object = children.get(0);
-					candidates = new HashSet<>();
-					candidates.add(object);
+	/**
+	 * Performs post-processing on the object, which might be split into more than
+	 * one part.
+	 * 
+	 * @param object - object to be post-processed
+	 * @return set of objects, which are derived from the original object
+	 */
+	public Set<Tree> objectPostProcessing(Tree origin) {
+		Set<Tree> objects = new HashSet<>();
+		objects.add(origin);
+		boolean changed = true;
+
+		while (changed) {
+			changed = false;
+			Set<Tree> iter = new HashSet<>();
+			for (Tree object : objects) {
+				List<Entry<Tree, String>> labels = object.getChildrenAsList().stream()
+						.map(child -> new AbstractMap.SimpleEntry<Tree, String>(child, child.label().toString()))
+						.collect(Collectors.toList());
+
+				if (labels.stream().anyMatch(x -> x.getValue().equals("CC"))) {
+					Set<String> separators = new HashSet<>();
+					separators.add("CC");
+					Set<Tree> split = splitObjectsByCC(labels, separators);
+					if (!split.isEmpty()) {
+						iter.addAll(split);
+						changed = true;
+					}
+				} else if (labels.stream().anyMatch(x -> x.getValue().equals("SBAR"))) {
+					// Choose first children as successor
+					// TODO: Check if first child of "SBAR" fork is always "NP"
+					Tree firstNP = object.firstChild();
+					if (firstNP != null) {
+						iter.add(firstNP);
+						changed = true;
+					}
+				} else if (labels.stream().anyMatch(x -> x.getValue().equals("DT"))) {
+					// Declare labels to be filtered
+					// TODO: Think about what tags should be added
+					Set<String> filter = new HashSet<>();
+					filter.add("DT");
+					filter.add("JJ");
+					// Remove nodes with a specific set of labels from the tree
+					List<Tree> filtered = labels.stream().filter(x -> !filter.contains(x.getValue()))
+							.map(entry -> entry.getKey()).collect(Collectors.toList());
+					// TODO: Check if "NP" is always the case
+					Tree artNode = new LabeledScoredTreeNode(new StringLabel("NP"), filtered);
+					iter.add(artNode);
+					changed = true;
+				} else {
+					// No changes on object
+					iter.add(object);
 				}
 			}
-		} else if (types.size() == 3) {
-			if (types.get(0).equals("NP") && types.get(1).equals("CC") && types.get(2).equals("NP")) {
-				candidates = new HashSet<>();
-				candidates.add(children.get(0));
-				candidates.add(children.get(2));
+			// Apply the changes of this iteration
+			if (changed) {
+				objects = iter;
 			}
 		}
-		if (candidates == null) {
-			candidates = new HashSet<>();
-			candidates.add(object);
+		return objects;
+	}
+
+	/**
+	 * Splits a sentence into parts by nodes with label contained by the set separator.
+	 * @param labels set of entries describing trees and their attached labels
+	 * @param separator set of labels to split by
+	 * @return set of trees, split by a separator 
+	 */
+	public Set<Tree> splitObjectsByCC(List<Entry<Tree, String>> labels, Set<String> separator) {
+		Set<Tree> result = new HashSet<>();
+		Set<List<Tree>> candidates = new HashSet<>();
+		List<Tree> current = new ArrayList<>();
+		for (Entry<Tree, String> entry : labels) {
+
+			if (separator.contains(entry.getValue())) {
+				// Do not add tree with label contained by separator
+				candidates.add(current);
+				current = new ArrayList<>();
+			} else {
+				current.add(entry.getKey());
+			}
 		}
-		return candidates;
+		candidates.add(current);
+
+		for (List<Tree> list : candidates) {
+			// Create an artificial parent for the nodes contained in the list
+			Tree artNode = new LabeledScoredTreeNode(new StringLabel("NP"), list);
+			result.add(artNode);
+		}
+		return result;
 	}
 
 	/**
@@ -208,7 +312,7 @@ public class TripletExtractor {
 			Tree node = stack.pop();
 			visited.putIfAbsent(node, false);
 			// Get type of node
-			String type = node.nodeString().split(" ")[0];
+			String type = node.label().toString();
 			if (match.contains(type)) {
 				result.add(node);
 			}
