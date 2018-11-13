73dbdfec0d85b6e1e4c8c97162f28ad1e2c2fcb8
Kristijan <kpanov@mail.upb.de>
2018-11-13 17:05:48 +0100

    creating structure

---
+++ knowledge_graph/main/java/com/group/proseminar/knowledge_graph/bindingfox/FoxBindingMain.java
@@ -0,0 +1,7 @@
+
+public class FoxBindingMain {
+  public static void main(final String[] a) throws Exception{
+    String input = "Barack Obama is born in May 12, 1955.";
+    FoxBinding.binding(input);
+  }
+}
